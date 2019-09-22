5675dc31be576e887f0532236fcd2e59f98d56da
adamkoz1 <adamkoz11@gmail.com>
2019-09-22 14:33:48 +0200

    Added let counter

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let number = prompt('Let give me some number?', '');
+
+if (number > 0) {
+    alert('1');
+} else if (number < 0) {
+    alert('-1');
+} else {
+    alert('0');
+}
+
+
+
+let a = 1;
+let b = 2;
+
+if (a + b < 4) {
+    result = 'Za mało';
+} else {
+    result = 'Za dużo';
+}
+
+(a + b) < 4 ? { result = 'Za mało' } : { result = 'Za dużo' };
+
+
+let a = +prompt('a?', '');
+
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+
+    case 1:
+
+    case 2:
+    case 3:
+}
+
+
+for (let i = 0; i < 100; i++) {
+    console.log('Warsztaty WarsawJS są świetne!');
+}
