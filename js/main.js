d65bd8656f6455a971c0ecee6a54e0823598562c
adamkoz1 <adamkoz11@gmail.com>
2019-09-22 13:51:26 +0200

    Added prompts...

--- js/main.js
+++ js/main.js
@@ -41,3 +41,12 @@ style.push('Rap');
 style.push('Reggae');
 
 console.log(style);
+
+
+let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+
+if (year == 2015) {
+  alert( 'You guessed it right!' );
+} else {
+  alert( 'How can you be so wrong?' ); // any value except 2015
+}
