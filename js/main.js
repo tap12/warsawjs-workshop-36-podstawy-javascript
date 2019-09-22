c28baf9fbab9a656cc5ff1d2668227b018256b68
adamkoz1 <adamkoz11@gmail.com>
2019-09-22 17:00:45 +0200

    Added chessboard.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 // let x = document.getElementById("myDIV");
+
 // let node = document.querySelector("h4").innerHTML = "Hello!!";
 
 // node.innerHTML("ssss");
 
 
 
-const h4 = document.querySelector('h4');
-const text = document.createElement('span');
-text.textContent = "jakiś tekst";
-h4.appendChild(text);
+// const h4 = document.querySelector('h4');
+// const text = document.createElement('span');
+// text.textContent = "jakiś tekst";
+// h4.appendChild(text);
+
+
+
+
+const main = document.querySelector('.main');
+const chessboard = document.createElement('section');
+chessboard.classList.add('chessboard');
+main.appendChild(chessboard);
+
+const board = document.querySelector('.chessboard');
+
+
+for (let i = 0; i < 64; i++) {
+    board.appendChild(document.createElement('div'))
+}
